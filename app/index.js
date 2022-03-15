e5b738d41f50a170c6643f4c32d63e75a66a9247
rvcas <x@rvcas.dev>
2022-03-15 11:57:06 -0400

    feat: some simple usage of pg

--- app/index.js
+++ app/index.js
@@ -1,10 +1,40 @@
 import express from "express";
+import pg from "pg";
 
-const app = express();
+const server = async () => {
+  const db = new pg.Client(process.env.DATABASE_URL);
+  await db.connect();
 
-app.get("/transactions-history-for-addresses", (req, res) => {});
-app.get("/check-addresses-in-use", (req, res) => {});
-app.get("/utxos-for-transactions", (req, res) => {});
-app.get("/best-block", (req, res) => {});
+  const app = express();
 
-app.listen(3000);
+  app.get("/transactions-history-for-addresses", (req, res) => {
+    await db.query(
+      `
+      SELECT "Transaction".id,
+        "Transaction".payload,
+        "Transaction".hash,
+        "Transaction".tx_index,
+        "Transaction".is_valid,
+        "Block".height
+      FROM "StakeCredential"
+      INNER JOIN "TxCredentialRelation" ON "TxCredentialRelation".credential_id = "StakeCredential".id
+      INNER JOIN "Transaction" ON "TxCredentialRelation".tx_id = "Transaction".id
+      INNER JOIN "Block" ON "Transaction".block_id = "Block".id
+      WHERE "StakeCredential".credential IN $1
+      ORDER BY "Block".height DESC,
+        "Transaction".tx_index ASC;
+    `,
+      [req.body.addresses]
+    );
+  });
+
+  app.get("/check-addresses-in-use", (req, res) => {});
+
+  app.get("/utxos-for-transactions", (req, res) => {});
+
+  app.get("/best-block", (req, res) => {});
+
+  app.listen(3000);
+};
+
+server();
