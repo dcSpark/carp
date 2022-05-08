802aaee137b6cbee9fab2958686c3d100fdc398f
Sebastien Guillemot <sebastiengllmt@gmail.com>
2022-05-09 02:34:21 +0900

    Split up ByronOutput to ByronAddress

--- indexer/src/tasks/byron/byron_address.rs
+++ indexer/src/tasks/byron/byron_address.rs
@@ -3,15 +3,12 @@ use std::sync::{Arc, Mutex};
 
 use entity::{
     prelude::*,
-    sea_orm::{prelude::*, DatabaseTransaction, Set},
+    sea_orm::{prelude::*, DatabaseTransaction},
 };
 use nameof::name_of_type;
-use pallas::{
-    codec::utils::MaybeIndefArray,
-    ledger::primitives::{
-        byron::{self, TxOut},
-        Fragment,
-    },
+use pallas::ledger::primitives::{
+    byron::{self},
+    Fragment,
 };
 use shred::{DispatcherBuilder, Read, ResourceId, System, SystemData, World, Write};
 use std::collections::BTreeMap;
@@ -20,32 +17,28 @@ use crate::tasks::{
     database_task::{
         BlockInfo, ByronTaskRegistryEntry, DatabaseTaskMeta, TaskBuilder, TaskRegistryEntry,
     },
-    era_common::{get_truncated_address, AddressInBlock},
+    era_common::AddressInBlock,
     utils::TaskPerfAggregator,
 };
 
-use super::{byron_address::ByronAddressTask, byron_txs::ByronTransactionTask};
+use super::byron_txs::ByronTransactionTask;
 
 #[derive(SystemData)]
 pub struct Data<'a> {
     byron_txs: Read<'a, Vec<TransactionModel>>,
-    byron_addresses: Read<'a, BTreeMap<Vec<u8>, AddressInBlock>>,
-    byron_outputs: Write<'a, Vec<TransactionOutputModel>>,
+    byron_addresses: Write<'a, BTreeMap<Vec<u8>, AddressInBlock>>,
 }
 
-pub struct ByronOutputTask<'a> {
+pub struct ByronAddressTask<'a> {
     db_tx: &'a DatabaseTransaction,
     block: BlockInfo<'a, byron::Block>,
     handle: &'a tokio::runtime::Handle,
     perf_aggregator: Arc<Mutex<TaskPerfAggregator>>,
 }
 
-impl<'a> DatabaseTaskMeta<'a, byron::Block> for ByronOutputTask<'a> {
-    const TASK_NAME: &'static str = name_of_type!(ByronOutputTask);
-    const DEPENDENCIES: &'static [&'static str] = &[
-        name_of_type!(ByronTransactionTask),
-        name_of_type!(ByronAddressTask),
-    ];
+impl<'a> DatabaseTaskMeta<'a, byron::Block> for ByronAddressTask<'a> {
+    const TASK_NAME: &'static str = name_of_type!(ByronAddressTask);
+    const DEPENDENCIES: &'static [&'static str] = &[name_of_type!(ByronTransactionTask)];
 
     fn new(
         db_tx: &'a DatabaseTransaction,
@@ -65,10 +58,10 @@ impl<'a> DatabaseTaskMeta<'a, byron::Block> for ByronOutputTask<'a> {
 struct ByronOutputTaskBuilder;
 impl<'a> TaskBuilder<'a, byron::Block> for ByronOutputTaskBuilder {
     fn get_name() -> &'static str {
-        ByronOutputTask::TASK_NAME
+        ByronAddressTask::TASK_NAME
     }
     fn get_dependencies() -> &'static [&'static str] {
-        ByronOutputTask::DEPENDENCIES
+        ByronAddressTask::DEPENDENCIES
     }
     fn add_task<'c>(
         &self,
@@ -79,16 +72,16 @@ impl<'a> TaskBuilder<'a, byron::Block> for ByronOutputTaskBuilder {
         perf_aggregator: Arc<Mutex<TaskPerfAggregator>>,
         _properties: &ini::Properties,
     ) {
-        let task = ByronOutputTask::new(db_tx, block, handle, perf_aggregator);
+        let task = ByronAddressTask::new(db_tx, block, handle, perf_aggregator);
         dispatcher_builder.add(task, Self::get_name(), Self::get_dependencies());
     }
 }
 
 inventory::submit! {
-    TaskRegistryEntry::Byron(ByronTaskRegistryEntry {name: ByronOutputTask::TASK_NAME, builder: &ByronOutputTaskBuilder })
+    TaskRegistryEntry::Byron(ByronTaskRegistryEntry {name: ByronAddressTask::TASK_NAME, builder: &ByronOutputTaskBuilder })
 }
 
-impl<'a> System<'a> for ByronOutputTask<'_> {
+impl<'a> System<'a> for ByronAddressTask<'_> {
     type SystemData = Data<'a>;
 
     fn run(&mut self, mut bundle: Data<'a>) {
@@ -100,10 +93,9 @@ impl<'a> System<'a> for ByronOutputTask<'_> {
                 self.db_tx,
                 self.block,
                 bundle.byron_txs.as_slice(),
-                &bundle.byron_addresses,
             ))
             .unwrap();
-        *bundle.byron_outputs = result;
+        *bundle.byron_addresses = result;
 
         self.perf_aggregator
             .lock()
@@ -116,12 +108,11 @@ async fn handle_outputs(
     db_tx: &DatabaseTransaction,
     block: BlockInfo<'_, byron::Block>,
     byron_txs: &[TransactionModel],
-    byron_addresses: &BTreeMap<Vec<u8>, AddressInBlock>,
-) -> Result<Vec<TransactionOutputModel>, DbErr> {
+) -> Result<BTreeMap<Vec<u8>, AddressInBlock>, DbErr> {
     match &block.1 {
         // Byron era had Epoch-boundary blocks for calculating stake distribution changes
         // they don't contain any txs, so we can just ignore them
-        byron::Block::EbBlock(_) => Ok(vec![]),
+        byron::Block::EbBlock(_) => Ok(BTreeMap::<Vec<u8>, AddressInBlock>::default()),
         byron::Block::MainBlock(main_block) => {
             let tx_outputs: Vec<_> = main_block
                 .body
@@ -132,42 +123,20 @@ async fn handle_outputs(
                 .collect();
 
             if tx_outputs.is_empty() {
-                return Ok(vec![]);
+                return Ok(BTreeMap::<Vec<u8>, AddressInBlock>::default());
             }
+            // insert addresses
+            let address_map = crate::tasks::era_common::insert_addresses(
+                &tx_outputs
+                    .iter()
+                    .flat_map(|pair| pair.0.iter())
+                    .map(|output| output.address.encode_fragment().unwrap())
+                    .collect(),
+                db_tx,
+            )
+            .await?;
 
-            // note: outputs have to be added before inputs
-            Ok(insert_byron_outputs(db_tx, byron_addresses, &tx_outputs).await?)
+            Ok(address_map)
         }
     }
-}
-
-async fn insert_byron_outputs(
-    txn: &DatabaseTransaction,
-    address_map: &BTreeMap<Vec<u8>, AddressInBlock>,
-    outputs: &[(&MaybeIndefArray<TxOut>, &TransactionModel)],
-) -> Result<Vec<TransactionOutputModel>, DbErr> {
-    let result = TransactionOutput::insert_many(
-        outputs
-            .iter()
-            .flat_map(|pair| pair.0.iter().enumerate().zip(std::iter::repeat(pair.1)))
-            .map(
-                |((output_index, output), tx_id)| TransactionOutputActiveModel {
-                    payload: Set(output.encode_fragment().unwrap()),
-                    address_id: Set(address_map
-                        .get(get_truncated_address(
-                            &output.address.encode_fragment().unwrap(),
-                        ))
-                        .unwrap()
-                        .model
-                        .id),
-                    tx_id: Set(tx_id.id),
-                    output_index: Set(output_index as i32),
-                    ..Default::default()
-                },
-            ),
-    )
-    .exec_many_with_returning(txn)
-    .await?;
-
-    Ok(result)
 }
--- indexer/src/tasks/byron/mod.rs
+++ indexer/src/tasks/byron/mod.rs
@@ -1,3 +1,4 @@
+pub mod byron_address;
 pub mod byron_executor;
 pub mod byron_inputs;
 pub mod byron_outputs;
