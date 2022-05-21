e16d4d56539eff58ef795c49c5beca6307a1ccb3
Sebastien Guillemot <sebastiengllmt@gmail.com>
2022-05-21 13:34:38 +0900

    Add more readonly variants

--- indexer/tasks/src/era_common.rs
+++ indexer/tasks/src/era_common.rs
@@ -2,7 +2,9 @@ use std::collections::BTreeSet;
 
 use entity::{
     prelude::*,
-    sea_orm::{entity::*, prelude::*, ColumnTrait, Condition, DatabaseTransaction, Set},
+    sea_orm::{
+        entity::*, prelude::*, ColumnTrait, Condition, DatabaseTransaction, QueryOrder, Set,
+    },
 };
 use std::collections::BTreeMap;
 
@@ -257,4 +259,47 @@ pub async fn block_from_hash(
         }
         Some(block) => block,
     })
+}
+
+pub async fn output_from_pointer(
+    db_tx: &DatabaseTransaction,
+    pointers: &[(i64 /* txid */, usize /* output index */)],
+) -> Result<Vec<TransactionOutputModel>, DbErr> {
+    // https://github.com/dcSpark/carp/issues/46
+    let mut output_conditions = Condition::any();
+    for (tx_id, output_index) in pointers.iter() {
+        output_conditions = output_conditions.add(
+            Condition::all()
+                .add(TransactionOutputColumn::TxId.eq(*tx_id))
+                .add(TransactionOutputColumn::OutputIndex.eq(*output_index as i32)),
+        );
+    }
+
+    let outputs = TransactionOutput::find()
+        .filter(output_conditions)
+        .order_by_asc(TransactionOutputColumn::Id)
+        .all(db_tx)
+        .await?;
+    Ok(outputs)
+}
+pub async fn input_from_pointer(
+    db_tx: &DatabaseTransaction,
+    pointers: &[(i64 /* txid */, usize /* input index */)],
+) -> Result<Vec<TransactionInputModel>, DbErr> {
+    // https://github.com/dcSpark/carp/issues/46
+    let mut input_conditions = Condition::any();
+    for (tx_id, input_index) in pointers.iter() {
+        input_conditions = input_conditions.add(
+            Condition::all()
+                .add(TransactionInputColumn::TxId.eq(*tx_id))
+                .add(TransactionInputColumn::InputIndex.eq(*input_index as i32)),
+        );
+    }
+
+    let inputs = TransactionInput::find()
+        .filter(input_conditions)
+        .order_by_asc(TransactionInputColumn::Id)
+        .all(db_tx)
+        .await?;
+    Ok(inputs)
 }
