a01e80b73e41ffc8e38288baca295a8a6625dec6
Eugene Gostkin <eugene@gostk.in>
2023-11-07 14:05:28 +0300

    Fix lints

--- indexer/migration/src/lib.rs
+++ indexer/migration/src/lib.rs
@@ -17,8 +17,8 @@ mod m20220528_000012_create_plutus_data_table;
 mod m20220808_000013_create_transaction_reference_input_table;
 mod m20221031_000014_create_dex_table;
 mod m20230223_000015_modify_block_table;
-mod m20231025_000016_projected_nft;
 mod m20230927_231206_create_stake_delegation_table;
+mod m20231025_000016_projected_nft;
 
 pub struct Migrator;
 
