4870a17a1f578f44d62bc405841dae752fd4dae3
Enzo Cioppettini <enzo.cioppettini@globant.com>
2023-12-21 18:03:15 -0300

    fix cip14 fingerprint hash

--- indexer/tasks/src/utils.rs
+++ indexer/tasks/src/utils.rs
@@ -11,8 +11,8 @@ pub fn blake2b256(data: &[u8]) -> [u8; 32] {
 }
 
 // https://github.com/cardano-foundation/CIPs/pull/64
-pub fn blake2b160(data: &[u8]) -> [u8; 32] {
-    let mut out = [0; 32];
+pub fn blake2b160(data: &[u8]) -> [u8; 20] {
+    let mut out = [0; 20];
     Blake2b::blake2b(&mut out, data, &[]);
     out
 }
