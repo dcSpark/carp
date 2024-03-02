9f49d2a89d01d0bc1123c27fd47155cba371d71b
Enzo Cioppettini <enzo.cioppettini@globant.com>
2024-03-01 22:08:54 -0300

    migrated projected nft range endpoint to block,tx pagination

--- webserver/server/app/models/projected_nft/projectedNftRangeByAddress.sql
+++ webserver/server/app/models/projected_nft/projectedNftRangeByAddress.sql
@@ -2,39 +2,38 @@
 @name sqlProjectedNftRangeByAddress
 */
 SELECT
-    encode("ProjectedNFT".owner_address, 'hex') as "owner_address!",
-
-    encode("ProjectedNFT".previous_utxo_tx_hash, 'hex') as previous_tx_hash,
-    "ProjectedNFT".previous_utxo_tx_output_index as previous_tx_output_index,
-
-    CASE
-        WHEN "TransactionOutput".output_index = NULL THEN NULL
-        ELSE "TransactionOutput".output_index
-        END AS action_output_index,
-
-    encode("Transaction".hash, 'hex') as "action_tx_id!",
-
-    "ProjectedNFT".policy_id as policy_id,
-    "ProjectedNFT".asset_name as asset_name,
-    "ProjectedNFT".amount as amount,
-
-    CASE
-        WHEN "ProjectedNFT".operation = 0 THEN 'Lock'
-        WHEN "ProjectedNFT".operation = 1 THEN 'Unlocking'
-        WHEN "ProjectedNFT".operation = 2 THEN 'Claim'
-        ELSE 'Invalid'
-        END AS "status!",
-
-    encode("ProjectedNFT".plutus_datum, 'hex') as "plutus_datum!",
-    "ProjectedNFT".for_how_long as for_how_long,
-
-    "Block".slot as action_slot
+    json_agg(json_build_object(
+        'ownerAddress', encode("ProjectedNFT".owner_address, 'hex'),
+        'previousUtxoTxHash', encode("ProjectedNFT".previous_utxo_tx_hash, 'hex'),
+        'previousTxOutputIndex', "ProjectedNFT".previous_utxo_tx_output_index,
+        'actionOutputIndex', CASE
+            WHEN "TransactionOutput".output_index = NULL THEN NULL
+            ELSE "TransactionOutput".output_index
+            END,
+        'actionTxId', encode("Transaction".hash, 'hex'),
+        'policyId', "ProjectedNFT".policy_id,
+        'assetName', "ProjectedNFT".asset_name,
+        'amount', "ProjectedNFT".amount,
+        'status', CASE
+            WHEN "ProjectedNFT".operation = 0 THEN 'Lock'
+            WHEN "ProjectedNFT".operation = 1 THEN 'Unlocking'
+            WHEN "ProjectedNFT".operation = 2 THEN 'Claim'
+            ELSE 'Invalid'
+            END,
+        'plutusDatum', encode("ProjectedNFT".plutus_datum, 'hex'),
+        'forHowLong', "ProjectedNFT".for_how_long,
+        'actionSlot', "Block".slot
+    )) as "payload!",
+    encode("Block".hash, 'hex') as "block!",
+    encode("Transaction".hash, 'hex') as "tx_id!"
 FROM "ProjectedNFT"
          LEFT JOIN "TransactionOutput" ON "TransactionOutput".id = "ProjectedNFT".hololocker_utxo_id
          JOIN "Transaction" ON "Transaction".id = "ProjectedNFT".tx_id
          JOIN "Block" ON "Transaction".block_id = "Block".id
 WHERE
-        encode("ProjectedNFT".owner_address, 'hex') = :owner_address!
-    AND "Block".slot > :min_slot!
-    AND "Block".slot <= :max_slot!
-ORDER BY ("Block".height, "Transaction".tx_index) ASC;
+    encode("ProjectedNFT".owner_address, 'hex') = :owner_address! AND
+	"Transaction".id > :after_tx_id! AND
+	"Transaction".id <= :until_tx_id!
+GROUP BY ("Block".id, "Transaction".id)
+ORDER BY ("Block".height, "Transaction".tx_index) ASC
+LIMIT :limit!;
