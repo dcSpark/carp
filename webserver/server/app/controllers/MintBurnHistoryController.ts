09280411f5e42fb595c0d4aba18310597e2cc524
Enzo Cioppettini <enzo.cioppettini@globant.com>
2024-03-04 00:43:33 -0300

    reformat MintBurnHistoryController and add top level doc

--- webserver/server/app/controllers/MintBurnHistoryController.ts
+++ webserver/server/app/controllers/MintBurnHistoryController.ts
@@ -7,145 +7,149 @@ import type { ErrorShape } from '../../../shared/errors';
 import type { EndpointTypes } from '../../../shared/routes';
 import { Routes } from '../../../shared/routes';
 import { mintBurnRange, mintBurnRangeByPolicyIds } from '../services/MintBurnHistoryService';
-import type { MintBurnHistoryRequest, MintBurnHistoryResponse, MintBurnSingleResponse } from "../../../shared/models/MintBurn";
-import type { PolicyId } from "../../../shared/models/PolicyIdAssetMap";
-import type { ISqlMintBurnRangeResult, ISqlMintBurnRangeByPolicyIdsResult } from "../models/asset/mintBurnHistory.queries";
+import type { MintBurnSingleResponse } from '../../../shared/models/MintBurn';
+import type { PolicyId } from '../../../shared/models/PolicyIdAssetMap';
+import type {
+  ISqlMintBurnRangeResult,
+  ISqlMintBurnRangeByPolicyIdsResult,
+} from '../models/asset/mintBurnHistory.queries';
 
 const route = Routes.mintBurnHistory;
 
 @Route('asset/mint-burn-history')
 export class MintRangeController extends Controller {
-    @SuccessResponse(`${StatusCodes.OK}`)
-    @Post()
-    public async projectedNftRange(
-        @Body()
-            requestBody: EndpointTypes[typeof route]['input'],
-        @Res()
-            errorResponse: TsoaResponse<
-            StatusCodes.BAD_REQUEST | StatusCodes.CONFLICT | StatusCodes.UNPROCESSABLE_ENTITY,
-            ErrorShape
-        >
-    ): Promise<EndpointTypes[typeof route]['response']> {
-        if (requestBody.policyIds !== undefined && requestBody.policyIds.length > 0) {
-            return await this.handle_by_policy_ids_query(requestBody.policyIds, requestBody);
-        } else {
-            return await this.handle_general_query(requestBody);
-        }
+  /**
+   * Gets mint and burn events in the provided slot range, optionally filtering
+   * by policyId(s). A burn event is a mint with a negative value.
+   *
+   */
+  @SuccessResponse(`${StatusCodes.OK}`)
+  @Post()
+  public async projectedNftRange(
+    @Body()
+    requestBody: EndpointTypes[typeof route]['input'],
+    @Res()
+    errorResponse: TsoaResponse<
+      StatusCodes.BAD_REQUEST | StatusCodes.CONFLICT | StatusCodes.UNPROCESSABLE_ENTITY,
+      ErrorShape
+    >
+  ): Promise<EndpointTypes[typeof route]['response']> {
+    if (requestBody.policyIds !== undefined && requestBody.policyIds.length > 0) {
+      return await this.handle_by_policy_ids_query(requestBody.policyIds, requestBody);
+    } else {
+      return await this.handle_general_query(requestBody);
     }
+  }
 
-    async handle_general_query(
-        requestBody: EndpointTypes[typeof route]['input'],
-    ): Promise<EndpointTypes[typeof route]['response']> {
-        const assets = await tx<
-            ISqlMintBurnRangeResult[]
-        >(pool, async dbTx => {
-            const data = await mintBurnRange({
-                range: requestBody.range,
-                dbTx
-            });
+  async handle_general_query(
+    requestBody: EndpointTypes[typeof route]['input']
+  ): Promise<EndpointTypes[typeof route]['response']> {
+    const assets = await tx<ISqlMintBurnRangeResult[]>(pool, async dbTx => {
+      const data = await mintBurnRange({
+        range: requestBody.range,
+        dbTx,
+      });
 
-            return data;
-        });
+      return data;
+    });
 
-        let mintRangeResponse: MintBurnSingleResponse = {
-            actionTxId: "",
-            actionBlockId: "",
-            metadata: null,
-            actionSlot: 0,
-            assets: {},
-        };
+    let mintRangeResponse: MintBurnSingleResponse = {
+      actionTxId: '',
+      actionBlockId: '',
+      metadata: null,
+      actionSlot: 0,
+      assets: {},
+    };
 
-        const result: MintBurnSingleResponse[] = [];
+    const result: MintBurnSingleResponse[] = [];
 
-        for (const entry of assets) {
-            const policyId = entry.policy_id !== null ? entry.policy_id.toString() : "";
-            const assetName = entry.asset_name !== null ? entry.asset_name.toString() : "";
-            const actionTxId = entry.action_tx_id !== null ? entry.action_tx_id.toString() : "";
-            const actionBlockId = entry.action_block_id !== null ? entry.action_block_id.toString() : "";
+    for (const entry of assets) {
+      const policyId = entry.policy_id !== null ? entry.policy_id.toString() : '';
+      const assetName = entry.asset_name !== null ? entry.asset_name.toString() : '';
+      const actionTxId = entry.action_tx_id !== null ? entry.action_tx_id.toString() : '';
+      const actionBlockId = entry.action_block_id !== null ? entry.action_block_id.toString() : '';
 
-            if (mintRangeResponse.actionTxId != actionTxId) {
-                if (mintRangeResponse.actionTxId.length > 0) {
-                    result.push(mintRangeResponse);
-                }
-
-                mintRangeResponse = {
-                    actionSlot: entry.action_slot,
-                    actionTxId: actionTxId,
-                    actionBlockId: actionBlockId,
-                    metadata: entry.action_tx_metadata,
-                    assets: {},
-                }
-            }
-
-            const for_policy = mintRangeResponse.assets[policyId] ?? {};
-
-            for_policy[assetName] = entry.amount;
-            mintRangeResponse.assets[policyId] = for_policy;
-        }
-
+      if (mintRangeResponse.actionTxId != actionTxId) {
         if (mintRangeResponse.actionTxId.length > 0) {
-            result.push(mintRangeResponse);
+          result.push(mintRangeResponse);
         }
 
-        return result;
-    }
-
-    async handle_by_policy_ids_query(
-        policyIds: PolicyId[],
-        requestBody: EndpointTypes[typeof route]['input'],
-    ): Promise<EndpointTypes[typeof route]['response']> {
-        const assets = await tx<
-            ISqlMintBurnRangeByPolicyIdsResult[]
-        >(pool, async dbTx => {
-            const data = await mintBurnRangeByPolicyIds({
-                range: requestBody.range,
-                policyIds: policyIds,
-                dbTx
-            });
-
-            return data;
-        });
-
-        let mintRangeResponse: MintBurnSingleResponse = {
-            actionTxId: "",
-            actionBlockId: "",
-            metadata: null,
-            actionSlot: 0,
-            assets: {},
+        mintRangeResponse = {
+          actionSlot: entry.action_slot,
+          actionTxId: actionTxId,
+          actionBlockId: actionBlockId,
+          metadata: entry.action_tx_metadata,
+          assets: {},
         };
+      }
 
-        const result: MintBurnSingleResponse[] = [];
+      const for_policy = mintRangeResponse.assets[policyId] ?? {};
 
-        for (const entry of assets) {
-            const policyId = entry.policy_id !== null ? entry.policy_id.toString() : "";
-            const assetName = entry.asset_name !== null ? entry.asset_name.toString() : "";
-            const actionTxId = entry.action_tx_id !== null ? entry.action_tx_id.toString() : "";
-            const actionBlockId = entry.action_block_id !== null ? entry.action_block_id.toString() : "";
-
-            if (mintRangeResponse.actionTxId != actionTxId) {
-                if (mintRangeResponse.actionTxId.length > 0) {
-                    result.push(mintRangeResponse);
-                }
-
-                mintRangeResponse = {
-                    actionSlot: entry.action_slot,
-                    actionTxId: actionTxId,
-                    actionBlockId: actionBlockId,
-                    metadata: entry.action_tx_metadata,
-                    assets: {},
-                }
-            }
-
-            const for_policy = mintRangeResponse.assets[policyId] ?? {};
-
-            for_policy[assetName] = entry.amount;
-            mintRangeResponse.assets[policyId] = for_policy;
-        }
-
-        if (mintRangeResponse.actionTxId.length > 0) {
-            result.push(mintRangeResponse);
-        }
-
-        return result;
+      for_policy[assetName] = entry.amount;
+      mintRangeResponse.assets[policyId] = for_policy;
     }
+
+    if (mintRangeResponse.actionTxId.length > 0) {
+      result.push(mintRangeResponse);
+    }
+
+    return result;
+  }
+
+  async handle_by_policy_ids_query(
+    policyIds: PolicyId[],
+    requestBody: EndpointTypes[typeof route]['input']
+  ): Promise<EndpointTypes[typeof route]['response']> {
+    const assets = await tx<ISqlMintBurnRangeByPolicyIdsResult[]>(pool, async dbTx => {
+      const data = await mintBurnRangeByPolicyIds({
+        range: requestBody.range,
+        policyIds: policyIds,
+        dbTx,
+      });
+
+      return data;
+    });
+
+    let mintRangeResponse: MintBurnSingleResponse = {
+      actionTxId: '',
+      actionBlockId: '',
+      metadata: null,
+      actionSlot: 0,
+      assets: {},
+    };
+
+    const result: MintBurnSingleResponse[] = [];
+
+    for (const entry of assets) {
+      const policyId = entry.policy_id !== null ? entry.policy_id.toString() : '';
+      const assetName = entry.asset_name !== null ? entry.asset_name.toString() : '';
+      const actionTxId = entry.action_tx_id !== null ? entry.action_tx_id.toString() : '';
+      const actionBlockId = entry.action_block_id !== null ? entry.action_block_id.toString() : '';
+
+      if (mintRangeResponse.actionTxId != actionTxId) {
+        if (mintRangeResponse.actionTxId.length > 0) {
+          result.push(mintRangeResponse);
+        }
+
+        mintRangeResponse = {
+          actionSlot: entry.action_slot,
+          actionTxId: actionTxId,
+          actionBlockId: actionBlockId,
+          metadata: entry.action_tx_metadata,
+          assets: {},
+        };
+      }
+
+      const for_policy = mintRangeResponse.assets[policyId] ?? {};
+
+      for_policy[assetName] = entry.amount;
+      mintRangeResponse.assets[policyId] = for_policy;
+    }
+
+    if (mintRangeResponse.actionTxId.length > 0) {
+      result.push(mintRangeResponse);
+    }
+
+    return result;
+  }
 }
